e7974c08497ec9dd3152981e1d4306fc62dd3778
nechoventsi <ventsi.v.dimitrov@gmail.com>
2015-03-04 20:19:33 +0200

    Added error messages

--- f-c-temperature.py
+++ f-c-temperature.py
@@ -3,21 +3,31 @@ print "What would you like to convert?"
 print "1: Fahrenheit to Celsius"
 print "2: Celsius to Fahrenheit"
 
-inp = raw_input ("Please, enter '1' or '2':\n")
+inp = raw_input ("Please, enter '1' or '2':")
 
 if inp == "1":
     F = raw_input ("Enter Fahrenheit temperature:\n")
-    F = float(F)
-    C = (F - 32) * 5/9
-    print "Celsius temperature:", C
+    
+    try:
+        F = float(F)
+        C = (F - 32) * 5/9
+        print "Celsius temperature:", C
 
+    except:
+        print "Error! Please enter numeric input!"
+    
 elif inp == "2":
     C = raw_input ("Enter Celsius temperature:\n")
-    C = float(C)
-    F = (C * 9/5) + 32
-    print "Fahrenheit temperature:", F
+
+    try:    
+        C = float(C)
+        F = (C * 9/5) + 32
+        print "Fahrenheit temperature:", F
+
+    except:
+        print "Error! Please enter numeric input!"
 
 else:
     print "Error! Please enter '1' or '2'!"
 
-# Version 0.3.1
+# Version 0.4.0
